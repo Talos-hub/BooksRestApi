96d03427df7ea9bdc1911f09d281cd65acdef63c
Talos <wishpersofthenine@gmail.com>
2025-09-10 17:32:35 +0700

    Created a construction for bookService

--- internal/services/BookService.go
+++ internal/services/BookService.go
@@ -4,7 +4,16 @@ import (
 	"github.com/Talos-hub/BooksRestApi/internal/abstraction"
 )
 
-type BookService struct {
-	logger  abstraction.Logger
-	storage abstraction.Storage
+// BookService impemented handlers for hadle book.
+// It contains logger and storage interface
+type bookService struct {
+	logger  abstraction.Logger  // Logger that needed for write logs
+	storage abstraction.Storage // it might be any strage for instance Postgresqls, Sqlite, json
+}
+
+func NewBookService(logger abstraction.Logger, storage abstraction.Storage) *bookService {
+	return &bookService{
+		logger:  logger,
+		storage: storage,
+	}
 }
