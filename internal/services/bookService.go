5fa42503b6435204a6cf41c4b5ef887d4d38b4ee
Talos <wishpersofthenine@gmail.com>
2025-09-11 16:15:39 +0700

    Created GetBooks, and GetBook

--- internal/services/bookService.go
+++ internal/services/bookService.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"github.com/Talos-hub/BooksRestApi/internal/abstraction"
+	"github.com/Talos-hub/BooksRestApi/internal/apperrors"
+	"github.com/Talos-hub/BooksRestApi/internal/models"
 )
 
 // BookService impemented handlers for hadle book.
@@ -17,4 +19,25 @@ func NewBookService(logger abstraction.Logger, storage abstraction.Storage) *boo
 		logger:  logger,
 		storage: storage,
 	}
+}
+
+// GetBooks returns all books from storage
+func (s *bookService) GetBooks() ([]models.Book, *apperrors.AppError) {
+	books, err := s.storage.GetAll()
+	if err != nil {
+		s.logger.Error("Error getting all books", "error", err)
+		return nil, apperrors.NewAppError(500, "error getting all books", err)
+	}
+	return books, nil
+}
+
+// GetBook return a book by id
+func (s *bookService) GetBook(id uint64) (models.Book, *apperrors.AppError) {
+	book, err := s.storage.GetById(id)
+	if err != nil {
+		s.logger.Error("Failed to get book by ID", "id", id, "error", err)
+		return models.Book{}, apperrors.NewAppError(404, "book not found", err)
+	}
+
+	return book, nil
 }
