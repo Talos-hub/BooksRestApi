c54501f9b9f9fa62ce49921190f05e41197bb3f8
Talos-hub <wishpersofthenine@gmail.com>
2025-09-17 17:33:36 +0700

    Created initTable, PostgresStorage implemented GetAll. Added db tags to models

--- internal/models/book.go
+++ internal/models/book.go
@@ -5,18 +5,18 @@ import (
 )
 
 type GeneralBook struct {
-	ID              uint64    `json:"id"`              // unique Id
-	Title           string    `json:"title"`           // Name of a Book
-	Genre           string    `json:"ganre"`           // for example Adnveture, Roman
-	PublicationDate time.Time `json:"publicationData"` // for instance 1970
-	Author          string    `json:"author"`
+	ID              uint64    `json:"id" db:"id"`                            // unique Id
+	Title           string    `json:"title" db:"title"`                      // Name of a Book
+	Genre           string    `json:"genre" db:"genre"`                      // for example Adnveture, Roman
+	PublicationDate time.Time `json:"publicationDate" db:"publication_date"` // for instance 1970
+	Author          string    `json:"author" db:"author"`
 }
 
 // Book is model that implemented behavior a real book.
 type Book struct {
-	General   GeneralBook `json:"general"`   // it is simple book
-	CreatedAt time.Time   `json:"createdAt"` // time when is was created
-	UpdatedAt time.Time   `json:"updateAt"`  // time when is was updated
+	General   GeneralBook `json:"general"`                   // it is simple book
+	CreatedAt time.Time   `json:"createdAt" db:"created_at"` // time when is was created
+	UpdatedAt time.Time   `json:"updateAt" db:"updated_at"`  // time when is was updated
 }
 
 type UpdateBookRequest struct {
--- internal/storages/postgresql/postgresql.go
+++ internal/storages/postgresql/postgresql.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/Talos-hub/BooksRestApi/internal/models"
 	"github.com/Talos-hub/BooksRestApi/internal/storages/config"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -13,7 +14,7 @@ type PostgresStorage struct {
 	config *config.DatabaseConfig
 }
 
-// TODO
+// NewPosgresStorage create new PostgresStorage that implemented Storage interface
 func NewPostgresStorage(config *config.DatabaseConfig) (*PostgresStorage, error) {
 	pgxconf, err := pgxpool.ParseConfig(config.ConnectionString())
 	if err != nil {
@@ -39,10 +40,110 @@ func NewPostgresStorage(config *config.DatabaseConfig) (*PostgresStorage, error)
 		return nil, fmt.Errorf("faild to ping database: %w", err)
 	}
 
-	//TODO create table book if it doesn't exist
+	// create a table book if it not exist
+	err = initTable(config, pool)
+	if err != nil {
+		return nil, err
+	}
 
 	return &PostgresStorage{
 		pool:   pool,
 		config: config,
 	}, nil
+}
+
+// initTable create a table if it not exist
+func initTable(config *config.DatabaseConfig, pool *pgxpool.Pool) error {
+	query := `
+	CREATE TABLE IF NOT EXISTS books (
+		id SERIAL PRIMARY KEY,
+		title VARCHAR(100) NOT NULL,
+		author VARCHAR(100) NOT NULL,
+		genre VARCHAR(100) NOT NULL,
+		publication_date TIMESTAMP NOT NULL,
+		created_at TIMESTAMP NOT NULL,
+		updated_at TIMESTAMP NOT NULL
+	);
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), config.Timeout)
+	defer cancel()
+
+	_, err := pool.Exec(ctx, query)
+	if err != nil {
+		return fmt.Errorf("faild to init database table: %w", err)
+	}
+
+	return nil
+
+}
+
+func (p *PostgresStorage) GetAll() ([]models.Book, error) {
+	query := `
+	SELECET
+		id,
+		title,
+		author,
+		ganre,
+		publication_date
+		created_at,
+		updated_at
+	FROM books
+	ORDER BY id
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), p.config.Timeout)
+	defer cancel()
+
+	//get amount of books
+	count, err := p.getCount(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := p.pool.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("faild to query books: %w", err)
+	}
+
+	defer rows.Close()
+
+	// created books avoid allocations
+	var books []models.Book = make([]models.Book, count)
+	var book models.Book
+
+	// it uses books[i] = book insted append because it more performance
+	// Direct assignment - very fast :
+	// No memory allocations during the loop,
+	// Direct memory access - O(1) time complexity
+	for i := 0; rows.Next(); i++ {
+		err := rows.Scan(
+			&book.General.ID,
+			&book.General.Title,
+			&book.General.Author,
+			&book.General.Genre,
+			&book.General.PublicationDate,
+			&book.CreatedAt,
+			&book.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("faild to scan books: %w", err)
+		}
+		books[i] = book
+	}
+
+	return books, nil
+}
+
+// Helper function to get total book count
+func (p *PostgresStorage) getCount(ctx context.Context) (int, error) {
+	query := `SELECT COUNT(*) FROM books`
+
+	var count int
+	err := p.pool.QueryRow(ctx, query).Scan(&count)
+	if err != nil {
+		return 0, fmt.Errorf("faild to get books count: %w", err)
+	}
+
+	return count, nil
+
 }
